bce981a51ca34adc5532d94b0e74db1a9b0de3fe
Farzad Vatandoust <97515569+FarzadVatandoust@users.noreply.github.com>
2022-01-23 21:36:50 +0330

    network architecture is complete

--- NN_train.py
+++ NN_train.py
@@ -1,19 +1,23 @@
 # import sys
 # sys.path.append('/content/drive/MyDrive/DLDNN/')
-
-from DLD_Util import DLD_Util as utl
+import os 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from keras.models import load_model
+import matplotlib.pyplot as plt
 import numpy as np
-from Alpha_NN import Autoencoder
+from DLD_Util import DLD_Util as utl
+from Alpha_NN import NeuralNetwork
 
 utl = utl()
 merged_data = utl.load_data('merged_data')
 
-aute = Autoencoder()
-Data_Norm, MAX_data = aute.preprocess(merged_data)
-np.save('MAX_data', MAX_data)
+NN = NeuralNetwork()
+Data_Norm, MAX_data = NN.preprocess(merged_data)
+#np.save('MAX_data', MAX_data)
 grid_size = Data_Norm[0][0].shape
-aute.create_model(grid_size, loss="mse", summary=False)
+label_size = Data_Norm[2][0].shape
 
+NN.create_model(grid_size, label_size, loss="mse", summary=True, plot=False)
 
 test_frac = 0.2
 train_ix = np.random.choice(len(Data_Norm[0]), size=int(
@@ -22,21 +26,30 @@ test_ix = np.setdiff1d(np.arange(len(Data_Norm[0])), train_ix)
 u_train, v_train, label_train = Data_Norm[0][train_ix], Data_Norm[1][train_ix], Data_Norm[2][train_ix]
 u_test, v_test, label_test = Data_Norm[0][test_ix], Data_Norm[1][test_ix], Data_Norm[2][test_ix]
 
-# aute.train(u_train, u_test, 10, batch_size=32)
-# aute.autoencoder.save('model_autoencod.h5')
+#
+NN.train_AutoE(u_train, u_test, 10, batch_size=32)
+NN.autoencoder.save('model_autoencoder.h5')
+NN.train_DLDNN(label_train, u_train, label_test, u_test, 20, batch_size=32)
+NN.DLDNN.save('model_DLDNN.h5')
 
-from keras.models import load_model
-M = load_model('model_autoencod.h5')
+AutE = load_model('model_autoencod.h5')
+DLDNN = load_model('model_DLDNN.h5')
+
+u_AutE = AutE.predict(u_test)[:,:,:,0]
+u_DLD = DLDNN.predict(label_test)[:,:,:,0]
+
+NN.display(u_test, u_AutE, u_DLD)
+
 
 # field prediction
 # due the inconsistancy of between our code and CNN we had to change array's shape
-test = M.predict(u_test)[:,:,:,0]
-aute.display(u_test, test)
+#test = M.predict(u_test)[:,:,:,0]
+#aute.display(u_test, test)
+
+
 
 
 #encoded_imgs = aute.encoder.predict(u_test)
 #aute.display(u_test, encoded_imgs[:,:,:,1])
 #decoded_imgs = aute.decoder.predict(encoded_imgs)
 #aute.display(u_test, decoded_imgs)
-
-
