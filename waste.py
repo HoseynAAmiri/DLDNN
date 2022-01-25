efa79e411dc86feecc5783c11fe4556bd845e7cd
Hoseyn Amiri <aamirihoseyn@gmail.com>
2022-01-25 10:30:24 +0330

    testing

--- waste.py
+++ waste.py
@@ -1,7 +1,8 @@
 from shapely.geometry import Point
 import matplotlib.pyplot as plt
 import numpy as np
-from shapely import affinity
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 from math import atan
 
 
