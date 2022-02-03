5c0fda4eeba81308737f7e83c566819afc58d976
Hoseyn Amiri <aamirihoseyn@gmail.com>
2022-02-03 21:29:41 +0330

    testing

---
+++ physical_neural_network.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+
+class PINN:
+    def __init__(self, x, y, D, N, G, Re):
+        
+        self.x = x
+        self.y = y
+        self.D = D
+        self.N = N
+        self.G = G
+        self.Re = Re
+        
+
+
+
+    
