35ad6a2d442972a5ef75cc628ed95df6c5748363
Hoseyn Amiri <aamirihoseyn@gmail.com>
2022-02-04 22:22:23 +0330

    testing

--- waste.py
+++ waste.py
@@ -3,10 +3,16 @@ import numpy as np
 from tqdm import tqdm
 import time
 
-z = [1, 2, 3, 4, 5]
-for i, i1 in zip(range(len(z)), tqdm(range(len(z)))):
+z = [1, 2, 3]
+
+
+for i, i1 in zip(range(len(z)-1), tqdm(range(len(z)-1), leave=True)):
+
     time.sleep(0.1)
-        
-    for ii, i2 in zip(range(len(z)), tqdm(range(len(z)), leave=False)):
-        
+    
+    pbar2 = tqdm(total=len(z), leave=True)    
+    for i2 in range(len(z)):
+        pbar2.update(1)
         time.sleep(1)
+    pbar2.close()    
+        
