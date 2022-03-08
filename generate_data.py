a8c09e50ca9c98181f823adcac9e854b29f405dd
Farzad Vatandoust <97515569+FarzadVatandoust@users.noreply.github.com>
2022-03-09 00:48:45 +0330

    Revert "h"
    This reverts commit e1a4342dbc3290417ee6f9083dad91648c2453d9.

--- generate_data.py
+++ generate_data.py
@@ -6,19 +6,10 @@ from tqdm import tqdm
 import numpy as np
 from DLD_env import Pillar
 from DLD_Utils import DLD_Utils as utl
-utl = utl()
-'''
-The settings that creates database
-D is the diameter of pillars
-G is the gap between pillars
-N is the periodic number of pillars lattice
-Re is the Reynols number
-'''
 
+def generate_data(simulator, f, N, Re):
 
-def generate_data(simulator, D, N, G, Re):
-
-    data_size = len(D)*len(N)*len(G)*len(Re)
+    data_size = len(f)*len(N)*len(Re)
 
     # Import COMSOL model
     client = mph.start()
@@ -31,53 +22,46 @@ def generate_data(simulator, D, N, G, Re):
 
     folder = cd + "\\Data"
     os.makedirs(folder)
-    info_D = list(map(str, D))
+    info_f = list(map(str, f))
     info_N = list(map(str, N))
-    info_G = list(map(str, G))
     info_Re = list(map(str, Re))
 
-    info_D.insert(0, 'D')
+    info_f.insert(0, 'f')
     info_N.insert(0, 'N')
-    info_G.insert(0, 'G')
     info_Re.insert(0, 'Re')
 
-    information = [info_D, info_N, info_G, info_Re]
+    information = [info_f, info_N, info_Re]
 
-    with open(folder + '\\information.csv', 'w') as f:
-        writer = csv.writer(f)
+    with open(folder + '\\information.csv', 'w') as file:
+        writer = csv.writer(file)
         writer.writerows(information)
 
     pbar = tqdm(total=data_size, position=0, leave=True)
-    for d in D:
-        folder = cd + "\\Data\\D{}".format(d)
+    for ff in f:
+        folder = cd + "\\Data\\D{}".format(ff)
         os.makedirs(folder)
-        for n in N:
-            for g in G:
-                for re in Re:
-                    # Set study's parameters
-                    param.set("D", str(d) + "[um]")
-                    param.set("N", str(n))
-                    param.set("G", str(g) + "[um]")
-                    param.set("Re", str(re))
+        for n in N:       
+            for re in Re:
+                # Set study's parameters
+                param.set("f", str(ff))
+                param.set("N", str(n))
+                param.set("Re", str(re))
 
-                    # Run model
-                    study.run()
+                # Run model
+                study.run()
 
-                    # Export data
-                    filename = cd + \
-                        "\\Data\\D{}\\{}_{}_{}_{}.csv".format(
-                            d, d, n, g, re)
-                    result.export("data1").set("filename", filename)
-                    result.export("data1").run()
+                # Export data
+                filename = cd + \
+                    "\\Data\\D{}\\{}_{}_{}.csv".format(
+                        ff, ff, n, re)
+                result.export("data1").set("filename", filename)
+                result.export("data1").run()
 
-                    pbar.update(1)
-                    time.sleep(0.1)
-
-# Changing raw data into data that are compatable to our neural network
-
+                pbar.update(1)
+                time.sleep(0.1)
 
 def compile_data(grid_size):
-
+# Changing raw data into data that are compatable to our neural network
     x_grid_size = grid_size[0]
     y_grid_size = grid_size[1]
 
@@ -93,9 +77,8 @@ def compile_data(grid_size):
     folders = [name for name in os.listdir(
         directory) if os.path.isdir(os.path.join(directory, name))]
 
-    dataset_psi = []
-    dataset_px = []
-    dataset_py = []
+    dataset_u = []
+    dataset_v = []
     labels = []
     for folder, p1 in zip(folders, tqdm(range(len(folders)))):
         folder_dir = directory + "\\" + folder
@@ -109,50 +92,38 @@ def compile_data(grid_size):
                 folder_dir + "\\" + name + ".csv", delimiter=",")
             
             label = list(map(float, name.split('_')))
-            d, n, g, re = label[0], label[1], label[2], label[3]
+            ff, n, re = label[0], label[1], label[2]
 
-            pillar = Pillar(d, n, g)
+            pillar = Pillar(ff, n)
 
             labels.append(label)
 
             x_mapped, y_mapped = utl.parall2square(
                 data[:, 0], data[:, 1], pillar)
 
-            psi_interp = utl.interp2grid(x_mapped, y_mapped, data[:, 2],
+            u_interp = utl.interp2grid(x_mapped, y_mapped, data[:, 2],
                                          x_grid, y_grid, recover=True)
 
-            p_interp = utl.interp2grid(x_mapped, y_mapped, data[:, 3],
+            v_interp = utl.interp2grid(x_mapped, y_mapped, data[:, 3],
                                        x_grid, y_grid, recover=True)
 
-            px, py = utl.gradient(p_interp, dx, dy)
-            px = utl.insert_mask(px, (x_grid, y_grid), pillar)
-            py = utl.insert_mask(py, (x_grid, y_grid), pillar)
-
             # Make dataset
-            dataset_psi.append(psi_interp)
-            dataset_px.append(px)
-            dataset_py.append(py)
+            dataset_u.append(u_interp)
+            dataset_v.append(v_interp)
 
             time.sleep(0.1)
 
-    return (np.array(dataset_psi), np.array(dataset_px), np.array(dataset_py), np.array(labels))
+    return (np.array(dataset_u), np.array(dataset_v), np.array(labels))
 
 if __name__ == "__main__":
-    '''
-    D = [10, 15, 20, 25, 30, 35, 40, 45, 50]
-    N = [3, 4, 5, 6, 7, 8, 9, 10]
-    G = [10, 15, 20, 25, 30, 35, 40, 45, 50]
-    Re = [0.01, 0.1, 0.2, 0.4, 0.8, 1, 2, 4, 6, 8, 10, 15, 20, 25, 30]
-    '''
 
-    D = [10, 20, 30, 40, 50]
+
+    ff = [10, 20, 30, 40, 50]
     N = [3, 5, 7, 9]
-    G = [10, 20, 30, 40, 50]
     RE = [0.01, 0.1, 0.2, 0.4, 0.8, 1]
-
     grid_size = (128, 128)
 
-    # generate_data('DLD_COMSOL.mph', D, N, G, RE)
+    generate_data('DLD_COMSOL.mph', D, N, G, RE)
 
     dataset = compile_data(grid_size)
     utl.save_data(dataset, 'dataset')
